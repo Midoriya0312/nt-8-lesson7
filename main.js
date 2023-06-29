397d42a3633c88e0cc41d25851e85d50d1f2f66e
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-29 13:42:58 +0500

    lesson7

---
+++ main.js
@@ -0,0 +1,47 @@
+// add2(4,7)
+
+// function add(x, y){
+//     return x + y
+// }
+
+// let add2 = function (x, y){
+//     return x + y
+// }
+
+// let add = (x) => x * x
+// add(3)
+
+// (function add() {
+//     return console.log(3 + 6);
+// })()
+
+
+
+// Homework
+
+
+// 1 misol
+
+// function multiply(a, b){
+//     return a * b
+//   }
+  
+//   multiply(4, 5)
+
+// 2 misol
+
+// function evenOrOdd(number) {
+//     if(number % 2 ==! 0){
+//       return "Odd"
+//     }else{
+//       return"Even"
+//     }
+//   }
+//   evenOrOdd(-7)
+
+// 3 misol
+
+
+
+
+// 4 misol
